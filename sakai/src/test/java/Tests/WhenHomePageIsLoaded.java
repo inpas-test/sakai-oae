c275a54b292c51e8c5d3bbacfcd4887a5b4bb63f
majorov76 <majorov76@gmail.com>
2012-12-28 11:57:39 +0400

    try to connect to github with name == 'git'

--- sakai/src/test/java/Tests/WhenHomePageIsLoaded.java
+++ sakai/src/test/java/Tests/WhenHomePageIsLoaded.java
@@ -34,7 +34,7 @@ public class WhenHomePageIsLoaded {
 		
 		System.out.println("Bigin test!");
 		
-		SearchResult result = pageHeader.search("sakai");
+		SearchResult result = pageHeader.search("123456");
 		
 		System.out.println(result.toString());
 		
